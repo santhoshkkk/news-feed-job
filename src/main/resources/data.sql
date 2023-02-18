f21ff31b56d8973eb281e911b935aff1ca4e5e03
Selva Kumar Anantha Raj <selanant@publicisgroupe.net>
2023-02-18 21:47:07 +0530

    Data Loader

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into newsjob(stock_name,search_keyword,source,job_last_run_time,job_next_run_time,created_date_time) values('reliance','reliance industries','bing',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP());
+insert into news-job(stock_name,search_keyword,source,job_last_run_time,job_next_run_time,created_date_time) values('reliance','reliance industries','bing',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP());
